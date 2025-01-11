7ebd9e79e7bf8deb520d40069fe9c173557296f6
Piotr Marcol <marcolpiotr@hotmail.com>
2025-01-11 23:46:18 +0100

    i18n

--- WMS_Frontend/src/assets/localization/i18nMain.js
+++ WMS_Frontend/src/assets/localization/i18nMain.js
@@ -1,9 +1,11 @@
 import en from './en.json'
 import pl from './pl.json'
+import cn from './cn.json'
 
 const i18nMesssages = {
     en: en,
     pl: pl,
+    cn: cn,
 }
 
 export default i18nMesssages

cc76034b646cb32131f5b4bfc71e8493b2a796c8
Piotr Marcol <marcolpiotr@hotmail.com>
2025-01-12 21:18:40 +0100

    Add HTTP port configuration and Tomcat connector setup

---
+++ WMS_Backend/src/main/java/com/inz/WMS_Backend/config/AppConfiguration.java
@@ -0,0 +1,23 @@
+package com.inz.WMS_Backend.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> httpRunner() {
+        return (TomcatServletWebServerFactory factory) -> {
+            final Connector connector = new Connector();
+            connector.setPort(httpPort);
+            factory.addAdditionalTomcatConnectors(connector);
+        };
+    }
+
+    @Value("${server.http.port}")
+    private int httpPort;
+}
